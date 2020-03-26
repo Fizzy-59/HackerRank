e881d421515f529388bdbe416794c2e8ffbd1d75
Romain <rom.dev@live.fr>
2020-03-26 17:39:21 +0100

    30 Days of Code : Day 1 - DONE

--- 30 Days of Code/day1 : Data Types.php
+++ 30 Days of Code/day1 : Data Types.php
@@ -8,25 +8,21 @@ $s = "HackerRank ";
 // Declare second integer, double, and String variables.
 // Read and save an integer, double, and String to your variables.
 
-$integer = 12;
+$integer = fgets($handle);
 
-$double = 4.0;
+$double = fgets($handle);
 
-$string = "is the best place to learn and practice coding!";
-
-
+$string = fgets($handle);
 
 // Print the sum of both integer variables on a new line.
 
+// Print the sum of the double variables on a new line.
 echo ($i + $integer) . "\n";
 
-// Print the sum of the double variables on a new line.
-
+// Concatenate and print the String variables on a new line
 echo number_format($d + $double, 1) . "\n";
 
-// Concatenate and print the String variables on a new line
-
-echo $s . $string;
 // The 's' variable above should be printed first.
+echo $s . $string;
 
 fclose($handle);
