d348db0851d8bba898a223d6377aa9ad366a3f39
Romain <rom.dev@live.fr>
2020-03-27 19:07:15 +0100

    Refactor

---
+++ Algorithms/Easy/4 - Diagonal Difference/diagonalDifference.php
@@ -0,0 +1,32 @@
+<?php
+// https://www.hackerrank.com/challenges/diagonal-difference/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference($arr) {
+    // Write your code here
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$arr = array();
+
+for ($i = 0; $i < $n; $i++) {
+    $arr_temp = rtrim(fgets(STDIN));
+
+    $arr[] = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+}
+
+$result = diagonalDifference($arr);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
