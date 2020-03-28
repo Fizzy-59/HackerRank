95a7d6a61bc443b3b053d66c695a10a84feabe11
Romain <rom.dev@live.fr>
2020-03-28 18:08:55 +0100

    Plus Minus - DONE

--- Algorithms/Easy/5 - Plus Minus/plusMinus.php
+++ Algorithms/Easy/5 - Plus Minus/plusMinus.php
@@ -3,6 +3,32 @@
 // Complete the plusMinus function below.
 function plusMinus($arr)
 {
+    $count = count($arr);
+
+    $positive = 0;
+    $negative = 0;
+    $zero = 0;
+
+    for($i = 0; $i < $count; $i++)
+    {
+        if($arr[$i] > 0)
+        {
+            $positive += 1;
+        }
+        elseif ($arr[$i] < 0)
+        {
+            $negative += 1;
+        }
+        else
+        {
+            $zero += 1;
+        }
+    }
+
+    echo round($positive/$count, $count) . "\n";
+    echo round($negative/$count, $count) . "\n";
+    echo round($zero/$count,     $count) . "\n";
+
 
 
 }
