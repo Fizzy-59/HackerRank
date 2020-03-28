90744ac65a03c7049dae0fbada8f4dc9f74cedac
Romain <rom.dev@live.fr>
2020-03-28 18:10:46 +0100

    Staircaise - WIP

---
+++ Algorithms/Easy/Staircase/staircase.php
@@ -0,0 +1,15 @@
+<?php
+
+// Complete the staircase function below.
+function staircase($n) {
+
+
+}
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+staircase($n);
+
+fclose($stdin);
