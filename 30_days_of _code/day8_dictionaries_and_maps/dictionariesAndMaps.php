d558925eaf4ee06cc064cfef9ed9857a74bb0eb2
Romain <rom.dev@live.fr>
2020-04-03 20:34:15 +0200

    Grading Student [WIP] - Day 9 : [WIP]

--- 30_days_of _code/day8_dictionaries_and_maps/dictionariesAndMaps.php
+++ 30_days_of _code/day8_dictionaries_and_maps/dictionariesAndMaps.php
@@ -16,14 +16,15 @@ $nb = $arr[0];
 
 for($i=1; $i<=$nb; $i++)
 {
-    $info = explode(' ', $arr[$i]); $phones[$info[0]] = $info[1];
+    $info = explode(' ', $arr[$i]);
+    $phones[$info[0]] = $info[1];
 }
 
-for($j=($nb+1); $j<count($arr); $j++)
+for($j=($nb+1); $j < count($arr); $j++)
 {
     if(array_key_exists($arr[$j], $phones))
     {
-        echo $arr[$j].'='.$phones[$arr[$j]]."\n";
+        echo $arr[$j] . '=' . $phones[$arr[$j]] . "\n";
     }
     else
     {
--- algorithms/easy/10_grading_students/gradingStudents.php
+++ algorithms/easy/10_grading_students/gradingStudents.php
@@ -1,1 +1,43 @@
 <?php
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents($grades)
+{
+    $count = count($grades);
+
+    for($i = 0; $i < $count; $i++)
+    {
+        if($grades[$i] > 38 && ($grades[$i] ‰ 5) > 3 )
+        {
+
+        }
+        else
+        {
+            echo "$grades[$i]\n";
+        }
+
+    }
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$grades_count = intval(trim(fgets(STDIN)));
+
+$grades = array();
+
+for ($i = 0; $i < $grades_count; $i++) {
+    $grades_item = intval(trim(fgets(STDIN)));
+    $grades[] = $grades_item;
+}
+
+$result = gradingStudents($grades);
+
+fwrite($fptr, implode("\n", $result) . "\n");
+
+fclose($fptr);
