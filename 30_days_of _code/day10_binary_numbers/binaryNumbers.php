3c978fc2b3a9b0aef2d595a5087c43ec90b119ae
Romain <rom.dev@live.fr>
2020-04-05 11:12:24 +0200

    Grading Student [STUCK] - Day10 : [DONE]

--- 30_days_of _code/day10_binary_numbers/binaryNumbers.php
+++ 30_days_of _code/day10_binary_numbers/binaryNumbers.php
@@ -1,5 +1,15 @@
 <?php
 
+$nb = (string) readline();
+$binary = decbin($nb);
+
+$ar = explode('0', $binary);
+
+$max = max($ar);
+$count = strlen($max);
+
+echo $count;
+
 
 
 $stdin = fopen("php://stdin", "r");
