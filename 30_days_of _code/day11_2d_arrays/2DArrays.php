7728c14a9de7710dbbb52e35fba88bf01b2477ae
Romain <rom.dev@live.fr>
2020-04-06 16:59:57 +0200

    Day11 : [WIP] - Day12 : [WIP] - Apple and Orange [WIP]

--- 30_days_of _code/day11_2d_arrays/2DArrays.php
+++ 30_days_of _code/day11_2d_arrays/2DArrays.php
@@ -1,6 +1,19 @@
 <?php
 
+$arr = [];
+$h = 0;
 
+for ($i = 0; $i < 4; $i++)
+{
+    for ($j = 0; $j < 4; $j++)
+    {
+        $arr[$h] =
+            arr[$i][$j] + arr[$i][$j+1] + arr[$i][$j+2]
+            + arr[$i+1][$j+1] + arr[$i+2][$j] + arr[$i+2][$j+1]
+            + arr[$i+2][$j+2];
+        $h++;
+    }
+}
 
 $stdin = fopen("php://stdin", "r");
 
