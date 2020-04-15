f4157aaac97ffcd9b42a32d768f1e13c3769ceb4
Romain <rom.dev@live.fr>
2020-04-15 16:55:20 +0200

    Day15 [WIP], Day16 [DONE]

--- 30_days_of _code/day16_exceptions_string_to_integer/exceptionsStringToInteger.php
+++ 30_days_of _code/day16_exceptions_string_to_integer/exceptionsStringToInteger.php
@@ -1,5 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
+$s = readline();
+
+function isString($s)
+{
+    if (!is_string($s))
+    {
+        throw new Exception('Bad String');
+    }
+    else
+    {
+        throw new Exception($s);
+    }
+}
+
+try
+{
+    isString($s);
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+
+
+
 $handle = fopen ("php://stdin","r");
 fscanf($handle,"%s",$S);
 
