3cc45acea889d1f1baaea47b2d96e1f5bd628975
Romain <rom.dev@live.fr>
2020-04-15 11:29:11 +0200

    Day15 [WIP]

---
+++ 30_days_of _code/day15_linked_list/linkedList.php
@@ -0,0 +1,43 @@
+<?php
+class Node
+{
+    public $data;
+    public $next;
+
+    function __construct($d)
+    {
+        $this->data = $d;
+        $this->next = NULL;
+    }
+}
+
+class Solution
+{
+
+    function insert($head, $data)
+    {
+        //complete this method
+    }
+
+    function display($head)
+    {
+        $start=$head;
+
+        while($start)
+        {
+            echo $start->data,' ';
+            $start=$start->next;
+        }
+    }
+}
+
+$T=intval(fgets(STDIN));
+$head=null;
+$mylist=new Solution();
+while($T-->0){
+    $data=intval(fgets(STDIN));
+    $head=$mylist->insert($head,$data);
+}
+$mylist->display($head);
+
+?>
